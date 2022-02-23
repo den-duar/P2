6954ccd1db5f6654cfa3fba1a3cd9e1078e41702
den-duar <2021211510079@iesp.edu.br>
2022-02-22 21:25:17 -0300

    Added some loop test

--- Front2/Variaveis.js
+++ Front2/Variaveis.js
@@ -4,3 +4,11 @@ var altura = 1.79;
 console.log(typeof nome);
 console.log(typeof idade);
 console.log(typeof altura);
+var i = 0;
+while (nome == "dennis") {
+    console.log(i);
+    i++
+    if (i >= 10) {
+        nome = "laura"
+    }
+}
