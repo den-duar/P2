4e6123ce7b90ce6eab8f2f7f9ace9acebbe213d9
den-duar <2021211510079@iesp.edu.br>
2022-03-15 21:22:31 -0300

    Better explanation of hoist on 1st commentary

--- Front2/Hoisting.js
+++ Front2/Hoisting.js
@@ -1,4 +1,4 @@
-console.log(nome);//Print before the var declaration. Works because of hoisting but value is "undefined"
+console.log(nome);//Print before the var declaration. Works because of hoisting on declaration, but value is "undefined"
 var nome = "ola turma";
 
 console.log(somarC(1,2));//Works because whole function declaration is hoisted up
