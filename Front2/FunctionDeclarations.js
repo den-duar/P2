bbe7ee11ac9a1281d79db350c1d31077273a2ed5
den-duar <2021211510079@iesp.edu.br>
2022-03-15 21:20:53 -0300

    Types of funcion declarations

---
+++ Front2/FunctionDeclarations.js
@@ -0,0 +1,11 @@
+function somarNumeros(n1, n2){ //Function declaration
+    return n1+n2;
+}
+
+console.log(somarNumeros(1,2));
+console.log(typeof somarNumeros);
+
+var somar = function(a,b){ //Function expression
+    return a+b;
+}
+console.log(somar(1,1));
