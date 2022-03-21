f6b638e2c7b7a0b5535db43e2dad9158aa069283
den-duar <2021211510079@iesp.edu.br>
2022-03-21 19:32:22 -0300

    Changed print text

--- DataStructures/MultiplicationTable.py
+++ DataStructures/MultiplicationTable.py
@@ -3,4 +3,4 @@ if numero >= 1 and numero <= 10:
     for multiplo in range(1,11):
         print (f"{numero} x {multiplo} = {numero*multiplo}" ) #String format, "f{value} parts of string {another value}"
 else:
-    print("Numero invalido.")
+    print("Número inválido.")
