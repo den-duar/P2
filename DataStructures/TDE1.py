e8c71b2d84dab272d356c67d8d51df27e122f0c3
den-duar <2021211510079@iesp.edu.br>
2022-03-21 19:20:08 -0300

    Better coding for TDE1

--- DataStructures/TDE1.py
+++ DataStructures/TDE1.py
@@ -1,9 +1,7 @@
-nota = -1
+nota = float(input("Informe uma nota de 0 a 10: "))
 while (nota < 0 or nota > 10):
-    nota = float(input("Informe uma nota de 0 a 10: "))
-    if(nota < 0 or nota > 10):
         nota = float(input("Nota invalida, informe nota entre 0 a 10: "))
-    else:
-        print("Nota cadastrada.")
+
+print("Nota cadastrada.")
 
     
