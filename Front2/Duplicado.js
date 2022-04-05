08ff24e5ca5a672a67493e1f906bf0674301522d
den-duar <90311867+den-duar@users.noreply.github.com>
2022-04-05 19:18:20 -0300

    Prova01

---
+++ Front2/Duplicado.js
@@ -0,0 +1,7 @@
+function duplicar(array = []){
+    for(i in array){
+        console.log(array[i]*2)
+    }
+}
+
+duplicar([2,4,5,30])
---
+++ Front2/ParOuImpar.js
@@ -0,0 +1,7 @@
+function parOuImpar(numero){
+    if(numero % 2 == 0){
+        console.log("Número par");
+    }else{
+        console.log("Número impar")
+    }
+}
