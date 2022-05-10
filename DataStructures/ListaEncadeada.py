f977f29e25c09e32c06a86eeec292428d178d66f
den-duar <90311867+den-duar@users.noreply.github.com>
2022-05-09 21:42:12 -0300

    Exemplo de estrutura de lista encadeada

---
+++ DataStructures/ListaEncadeada.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+
+    def mostrarNo(self):
+        print(self.valor)
+
+class ListaEncadeada:
+    def __init__(self):
+        self.primeiro = None
+        
+    
+    def insereInicio(self, valor):
+        novo = No(valor)
+        novo.proximo = self.primeiro
+        self.primeiro = novo
+
+    def mostrar(self):
+        if self.primeiro == None:
+            print("A lista está vazia")
+        
+        atual = self.primeiro
+        while atual != None:
+            atual.mostrarNo()
+            atual = atual.proximo
+    
+    def excluirInicio(self):
+        temp  = self.primeiro
+        if self.primeiro.proximo == None: ############################
+            return None
+        self.primeiro = self.primeiro.proximo
+        return temp
+    
+    def pesquisar(self, valor):
+        if self.primeiro == None:
+            print("A lista está vazia")
+            return None
+        atual = self.primeiro
+        while atual.valor != valor:
+            if atual.proximo == None:
+                return None
+            else:
+                atual = atual.proximo
+        return valor
+    
+    def excluirPosicao(self, valor):
+        if self.primeiro == None:
+            print("A lista está vazia")
+            return None
+        atual = self.primeiro
+        anterior = self.primeiro
+        while atual.valor != valor:
+            if atual.proximo == None:
+                return None
+            else:
+                anterior = atual
+                atual = atual.proximo
+        if atual == self.primeiro:
+            self.primeiro = self.primeiro.proximo
+        else:
+            anterior.proximo = atual.proximo
+        return atual 
+            
+        
