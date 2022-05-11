e60a2c48221dc3075b60d09a7cfbde13c19cabdc
den-duar <90311867+den-duar@users.noreply.github.com>
2022-05-10 21:27:05 -0300

    Modificação de tabela para "zebra-la" integrando html,css,js

---
+++ Front2/Tabela Zebrada/Tabela.js
@@ -0,0 +1,8 @@
+var tabela = document.getElementsByTagName("tr")
+for (var i = 0; i < tabela.length; i++) {
+    if (i % 2 == 0) {
+        tabela[i].setAttribute("class", "corAzul")
+    } else {
+        tabela[i].setAttribute("class", "corVerde")
+    }
+}
