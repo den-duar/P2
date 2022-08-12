7d4ac8a4485a2c4b32040e7cbfd600a4cca0689f
den-duar <90311867+den-duar@users.noreply.github.com>
2022-08-11 21:37:28 -0300

    Updating specific file creation

--- BD/SQLQueryBD2.sql
+++ BD/SQLQueryBD2.sql
@@ -20,6 +20,57 @@ create database clientes
 	MAXSIZE = 20,
 	FILEGROWTH = 5)
 
+create database vendas
+	ON (NAME = vendas_dat,
+	FILENAME = 'D:\SQLSV\vendas_bd.mdf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5)
+	LOG ON (NAME = vendas_log,
+	FILENAME = 'D:\SQLSV\vendas_bd.ldf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5)
 
-
+create database produtos
+	ON (NAME = produtos1_dat,
+	FILENAME = 'D:\SQLSV\produtos1_bd.mdf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5),
+	(NAME = produtos2_dat,
+	FILENAME = 'D:\SQLSV\produtos2_bd.ndf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5),
+	(NAME = produtos3_dat,
+	FILENAME = 'D:\SQLSV\produtos3_bd.ndf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5),
+	(NAME = produtos4_dat,
+	FILENAME = 'D:\SQLSV\produtos4_bd.ndf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5)
+	LOG ON (NAME = produtos1_log,
+	FILENAME = 'D:\SQLSV\produtos1_bd.ldf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5),
+	(NAME = produtos2_log,
+	FILENAME = 'D:\SQLSV\produtos2_bd.ldf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5),
+	(NAME = produtos3_log,
+	FILENAME = 'D:\SQLSV\produtos3_bd.ldf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5),
+	(NAME = produtos4_log,
+	FILENAME = 'D:\SQLSV\produtos4_bd.ldf',
+	SIZE = 5,
+	MAXSIZE = 20,
+	FILEGROWTH = 5)
 
